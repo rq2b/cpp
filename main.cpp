a7585fbc3c7621c03b5a538806caef580e9b20fb
rq2b <sankalysenko@gmail.com>
2026-05-31 14:09:45 +0300

    057

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+// Создать небольшое приложение для перевода сантиметров  дюймы. 
+// Приложение запрашивает у пользователя сантиметры и выводит размер в дюймах.
+
+// константа на этапе компиляции
+constexpr double CM_PER_INCH = 2.54;
+
 int main() {
-  std::cout << "Hello, world!\n";
+  std::cout << "Конвертация сантиметров -> дюймы\n";
+  std::cout << "Введите кол-во сантиметров: ";
+
+  double centimeters;
+
+  // стандартный ввод
+  // std::cin >> centimeters;
+
+  // ввод с проверкой валидности
+  if (!(std::cin >> centimeters)) {
+    std::cerr << "Ошибка; Введите число!\n";
+    return 1;
+  }
+
+  std::cout << centimeters << " см. в дюймах: " << centimeters / CM_PER_INCH << '\n';
+  // printf("%f см. в дюймах: %f\n", centimeters, inches);  // альтернативный подход с printf
+  
   return 0;
 }
